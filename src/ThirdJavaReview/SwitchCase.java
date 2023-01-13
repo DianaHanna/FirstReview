6ca79aba0b577404345551a69c8de9400e3403d9
DianaHanna <shehata.dianan@gmail.com>
2023-01-12 20:35:05 -0500

    pushing ThirdReview Codes

---
+++ src/ThirdJavaReview/SwitchCase.java
@@ -0,0 +1,24 @@
+package ThirdJavaReview;
+
+public class SwitchCase {
+
+    public static void main(String[] args) {
+
+        int worldCup=2022;
+
+        switch (worldCup){
+
+            case 2022:
+                System.out.println("Winner is Argentina");
+                break;
+            case 2018:
+                System.out.println("Winner is France");
+                break;
+            case 2014:
+                System.out.println("Winner is Germany");
+                break;
+            default:
+                System.out.println("Winner is unknown");
+        }
+    }
+}
